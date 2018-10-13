74d53c236a81ab3eabc9e3da020316344062714e
Edward Chan <edo@Edwards-MacBook-Pro.local>
2018-10-13 17:17:38 -0400

    init

--- .cache/sync-requires.js
+++
@@ -1,9 +0,0 @@
-// prefer default export if available
-const preferDefault = m => m && m.default || m
-
-
-exports.components = {
-  "component---src-pages-app-js": preferDefault(require("/Users/edo/Documents/git/schan3ed.github.io/src/pages/App.js")),
-  "component---src-pages-index-js": preferDefault(require("/Users/edo/Documents/git/schan3ed.github.io/src/pages/index.js"))
-}
-
