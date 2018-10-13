cef93f79e7e5989293d20fac8eb46f627ea12265
Edward Chan <edo@Edwards-MacBook-Pro.local>
2018-10-13 17:02:15 -0400

    init

--- portfolio/gatsby-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    siteMetadata: {
-      title: `Edward Chan`,
-      siteUrl: `https://schan3ed.github.io/`,
-      description: `Test Portfolio`,
-      pathPrefix: `/schan3ed.github.io`,
-    },
-}
